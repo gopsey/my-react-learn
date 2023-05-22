6fba4de36c4a5dbe0d3c58818b6646f72e094451
Gopalakrishnan <gopalkris96@gmail.com>
2023-05-22 10:40:00 +0530

    new files added

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+   return (<>
+      <div>Date</div>
+      <div>
+         <h2>Type</h2>
+         <div>Amount</div>
+      </div>
+   </>);
+}
+
+export default ExpenseItem;
