e12fcc5718ee42e4d068ac192f535b96143f1ec3
Gopalakrishnan <gopalakrishnan.balasubramanian@fiserv.com>
2023-06-14 20:19:33 +0530

    filter changes and card component

---
+++ src/components/Expenses/ExpenseFilter.js
@@ -0,0 +1,18 @@
+const ExpenseFilter = ({onExpenseFilterChange}) => {
+    const expenseFilterChangeHandler = (event) => {
+        const inputFilterValue = event.target.value;
+        onExpenseFilterChange(inputFilterValue);
+    }
+    return (
+        <>
+            <select type="dropdown" onChange={expenseFilterChangeHandler}>
+                <option value="2020">2020</option>
+                <option value="2021">2021</option>
+                <option value="2022">2022</option>
+                <option value="2023">2023</option>
+            </select>
+        </>
+    )
+}
+
+export default ExpenseFilter;
