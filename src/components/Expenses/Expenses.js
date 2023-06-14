79e901556687a8a2e7f3ad7e5569d8e5c56027e6
Gopalakrishnan <gopalakrishnan.balasubramanian@fiserv.com>
2023-06-14 20:19:21 +0530

    filter changes and card component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,26 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
+import Card from "../UI/Card";
+import { useState } from "react";
 
 const Expenses = ({expenses}) => {
+   const [filteredYear, setFilteredYear] = useState('2020');
+   const onExpenseFilterChange = (expenseFilterChangedValue) => {
+      setFilteredYear(expenseFilterChangedValue);
+      console.log(filteredYear);
+   }
    return (
-      <div className="expenses">
-         {
-            expenses.map(ele => <ExpenseItem expenses={ele} />)
-         }
-      </div>
+      <>
+         <Card className="expenses">
+            <ExpenseFilter selected={filteredYear} onExpenseFilterChange={onExpenseFilterChange} />
+            <div>
+               {
+                  expenses.map(ele => <ExpenseItem expenses={ele} />)
+               }
+            </div>
+         </Card>
+      </>
    );
 }
 
